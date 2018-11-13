82009cfd8aba8b7cdb9b832a8aa643f1d48eb28e
Liam Roberts <liam_r_123@hotmail.co.uk>
2018-11-13 13:06:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ function theBeatlesPlay(musicians, instruments){
 
   return beatles;
 }
+
+function johnLennonFacts(array){
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+  ];
+
+  while (i < 4){
+    return facts[i] + "!!!";
+    i++
+    
+  }
+  
+}

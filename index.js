5ddd5d23f13ea3d79cc804f63c848af1ecc5b9f6
Liam Roberts <liam_r_123@hotmail.co.uk>
2018-11-13 13:14:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,19 +8,20 @@ function theBeatlesPlay(musicians, instruments){
   return beatles;
 }
 
-function johnLennonFacts(array){
-  const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-  ];
+const facts = [
+"He was the last Beatle to learn to drive",
+"He was never a vegetarian",
+"He was a choir boy and boy scout",
+"He hated the sound of his own voice"
+];
 
+function johnLennonFacts(facts){
+  var array = []
   let i = 0;
 
   while (i < 4){
     array.push`${facts[i]}` + "!!!";
     i++;
   }
-  return array;
+  return array
 }
